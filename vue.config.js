f8968affb3513e16eee8ddb04d62bd5d302c6baa
Neeraj Saini <neerajsaini3071@gmail.com>
2026-02-10 15:43:53 -0500

    Fix GitHub Pages path configuration

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   transpileDependencies: [],
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-invoice-app/'
+    ? '/InvoicePro/'
     : '/'
 }
