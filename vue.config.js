691d288ba2a77707470a2766674dedcb0ccd385d
Neeraj Saini <neerajsaini3071@gmail.com>
2026-02-10 15:43:00 -0500

    Configure deployment for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   transpileDependencies: [],
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-invoice-app/'
+    : '/'
 }
